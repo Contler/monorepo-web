58ec4e399d1505883d12fe3cb3ba9b99dc9d7600
Jose G Vargas <ruedagato@gmail.com>
2019-11-16 16:39:09 -0500

    feat: add material modules

---
+++ apps/hotel/src/material/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+const materialModules = [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ...materialModules],
+  exports: materialModules,
+})
+export class MaterialModule {}
