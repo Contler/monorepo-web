01ba209463242fabccd66ff0cd425e8776ddd708
Jose G Vargas <ruedagato@gmail.com>
2019-11-21 17:02:51 -0500

    feat: add core service

--- apps/hotel/src/home/home.module.ts
+++ apps/hotel/src/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from 'hotel/home/home-routing.module';
 import { AdminHomeComponent } from 'hotel/home/page/admin-home/admin-home.component';
 import { MaterialModule } from 'hotel/material/material.module';
+import { EmployerComponent } from './page/employer/employer.component';
 
 @NgModule({
-  declarations: [AdminHomeComponent],
+  declarations: [AdminHomeComponent, EmployerComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
