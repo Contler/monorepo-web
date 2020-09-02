f72bf4549352cf6b0892869302347e2140f700b9
Jose G Vargas <ruedagato@gmail.com>
2020-09-02 18:18:33 -0500

    update build plugin

--- tools/plugins/stop-build.js
+++ tools/plugins/stop-build.js
@@ -1,5 +1,19 @@
 module.exports = {
-  onPreBuild: () => {
-    console.log('init build **************')
+  onPreBuild: ({utils: {build}}) => {
+    const currentProject = 'hotel';
+    const projectChange = checkRunBuild(currentProject)
+    if(!projectChange) {
+      build.cancelBuild(
+        `Build was cancelled because ${currentProject} was not affected by the latest changes`
+      );
+    }
   }
+}
+
+function checkRunBuild(project) {
+  const execSync = require('child_process').execSync;
+  const command = 'nx affected:apps --plain'
+  const output = execSync(command).toString().replace('\n', '')
+  const apps = output.split(' ')
+  return apps.includes(project)
 }
