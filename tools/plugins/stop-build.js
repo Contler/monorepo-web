b2e1bebbef1533f3375866dae61f13e0f6227bc7
Jose G Vargas <ruedagato@gmail.com>
2020-09-02 18:47:26 -0500

    check deploy hotel 5

--- tools/plugins/stop-build.js
+++ tools/plugins/stop-build.js
@@ -2,7 +2,6 @@ module.exports = {
   onPreBuild: ({utils: {build}}) => {
     const currentProject = process.env.PROJECT_NAME;
     const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
-    console.log(lastDeployedCommit)
     const latestCommit = 'HEAD';
     const projectChange = checkRunBuild(currentProject,  lastDeployedCommit, latestCommit)
     if(!projectChange) {
@@ -15,7 +14,9 @@ module.exports = {
 
 function checkRunBuild(project, fromHash, toHash) {
   const execSync = require('child_process').execSync;
-  const command = `nx affected:apps --plain --base=${fromHash} --head=${toHash}`
+  // const command = `nx affected:apps --plain --base=${fromHash} --head=${toHash}`
+  const command = `nx affected:apps --plain`
+  console.log(command)
   const output = execSync(command).toString().replace('\n', '')
   console.log(output);
   const apps = output.split(' ')
