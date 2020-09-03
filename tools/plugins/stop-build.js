3cae458374d37f65edff5197b9ec93c66948f386
Jose G Vargas <ruedagato@gmail.com>
2020-09-02 19:00:02 -0500

    check deploy hotel 9

--- tools/plugins/stop-build.js
+++ tools/plugins/stop-build.js
@@ -15,8 +15,8 @@ module.exports = {
 function checkRunBuild(project, fromHash, toHash) {
   const execSync = require('child_process').execSync;
   console.log(execSync('ls').toString());
-  const command = `nx affected:apps --plain --base=${fromHash} --head=${toHash}`
-  // const command = `nx affected:apps --plain`
+  // const command = `nx affected:apps --plain --base=${fromHash} --head=${toHash}`
+  const command = `nx affected:apps --plain --base=cf831f0a897ab08824643f6ea0505760851c2b4f --head=835b6efb12f7e71306bebd4b33176abc7abdf454`
   console.log(command)
   const output = execSync(command).toString().replace('\n', '')
   const apps = output.split(' ')
