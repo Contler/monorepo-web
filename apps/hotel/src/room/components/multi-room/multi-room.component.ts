21a433e783f9978bb37eccef51c809fcae790b31
Jose G Vargas <ruedagato@gmail.com>
2020-09-04 22:39:59 -0500

    update logic rooms range

--- apps/hotel/src/room/components/multi-room/multi-room.component.ts
+++ apps/hotel/src/room/components/multi-room/multi-room.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'contler-multi-room',
@@ -12,8 +19,30 @@ export class MultiRoomComponent {
 
   constructor(formBuild: FormBuilder) {
     this.roomGroup = formBuild.group({
-      init: ['', [Validators.required]],
-      end: ['', [Validators.required]],
+      init: ['', [Validators.required, this.checkRooms.bind(this)]],
+      end: ['', [Validators.required, this.checkRooms.bind(this)]],
     });
+    this.endRoom.valueChanges.subscribe(() => {
+      this.initRoom.updateValueAndValidity();
+    });
+  }
+
+  checkRooms(): ValidationErrors | null {
+    if (!this.roomGroup || !this.initRoom.value || !this.endRoom.value) {
+      return null;
+    }
+    return Number(this.endRoom.value) > Number(this.initRoom.value)
+      ? Number(this.endRoom.value) - Number(this.initRoom.value) < 20
+        ? null
+        : { maxRooms: 'max Rooms' }
+      : { badRooms: 'init room bad' };
+  }
+
+  get initRoom() {
+    return this.roomGroup.get('init');
+  }
+
+  get endRoom() {
+    return this.roomGroup.get('end');
   }
 }
