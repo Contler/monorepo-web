3ced57fee6c6616e14a663ec8d0798e3a44c7bd1
Jose G Vargas <ruedagato@gmail.com>
2020-09-17 15:51:28 -0500

    test only check

--- bin/deploy.js
+++ bin/deploy.js
@@ -2,23 +2,26 @@ const { execSync, spawn } = require('child_process');
 
 const project = process.argv[2];
 
-const respond = execSync('npm run affected:apps -- --plain').toString();
-const projectsAffected = respond.split('\n')[4].split(' ');
+const respond = execSync(
+  'npm run affected:apps -- --base="$BEFORE_COMMIT" --head="$AFTHER_COMMIT" --plain ',
+).toString();
 
-if (projectsAffected.includes(project)) {
-  const command = spawn('firebase', [
-    'deploy',
-    '--token',
-    '"$FIREBASE_TOKEN"',
-    '--project',
-    '"$FIREBASE_PROJECT"',
-    '--only',
-    `hosting:${project}`,
-  ]);
-  command.stdout.on('data', (data) => {
-    console.log(data.toString());
-  });
-  command.on('close', () => console.log('deploy complete'));
-} else {
-  console.log(`${project} no deployed`);
-}
+// const projectsAffected = respond.split('\n')[4].split(' ');
+//
+// if (projectsAffected.includes(project)) {
+//   const command = spawn('firebase', [
+//     'deploy',
+//     '--token',
+//     '"$FIREBASE_TOKEN"',
+//     '--project',
+//     '"$FIREBASE_PROJECT"',
+//     '--only',
+//     `hosting:${project}`,
+//   ]);
+//   command.stdout.on('data', (data) => {
+//     console.log(data.toString());
+//   });
+//   command.on('close', () => console.log('deploy complete'));
+// } else {
+//   console.log(`${project} no deployed`);
+// }
