a4b6a708b479f003dc7707584535cf5356499aad
Jose G Vargas <ruedagato@gmail.com>
2020-09-24 15:06:01 -0500

    add service to create money exchange

--- apps/guest/src/reception/const/exchange.const.ts
+++ apps/guest/src/reception/const/exchange.const.ts
@@ -1,1 +1,10 @@
-export const MONEY_OPTIONS = ['US Dollar'];
+export const MONEY_OPTIONS = [
+  {
+    name: 'US Dollar',
+    symbol: '$',
+  },
+  {
+    name: 'Euro',
+    symbol: '€',
+  },
+];
--- apps/guest/src/reception/pages/exchange/exchange.component.ts
+++ apps/guest/src/reception/pages/exchange/exchange.component.ts
@@ -1,6 +1,17 @@
 import { Component } from '@angular/core';
-import { MONEY_OPTIONS } from '../../const/exchange.const';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ReceptionService } from '@contler/core';
+import { GuestEntity } from '@contler/entity';
+import { ExchangeReqModel } from '@contler/models';
+
+import { MONEY_OPTIONS } from '../../const/exchange.const';
+import { GuestService } from '../../../services/guest.service';
+import { map, switchMap } from 'rxjs/operators';
+import { ModalConfigModel } from '../../../../../../libs/models/modal-config.model';
+import { ModalCompleteComponent } from '../../../common-components/modal-complete/modal-complete.component';
 
 @Component({
   selector: 'contler-exchange',
@@ -11,8 +22,16 @@ export class ExchangeComponent {
   readonly moneyOptions = MONEY_OPTIONS;
 
   changeForm: FormGroup;
+  symbol: string;
+  load = false;
 
-  constructor(formBuild: FormBuilder) {
+  constructor(
+    formBuild: FormBuilder,
+    private receptionService: ReceptionService,
+    private guestService: GuestService,
+    private dialog: MatDialog,
+    private router: Router,
+  ) {
     this.changeForm = formBuild.group({
       money: ['', Validators.required],
       value: ['', Validators.required],
@@ -20,6 +39,36 @@ export class ExchangeComponent {
   }
 
   save() {
-    console.log('save');
+    this.load = true;
+    const { money, value } = this.changeForm.value;
+    const modalConf: ModalConfigModel = {
+      text:
+        'Your currency exchange request has been succesfully received. Come to the reception to receive your money.',
+      close: 'Got it!',
+      icon: 'fas fa-check-circle',
+    };
+    this.guestService.$guest
+      .pipe(
+        map((guest) => this.generateExchangeReq(guest, money, value)),
+        switchMap((req) => this.receptionService.createExchangePetition(req)),
+        switchMap(() => this.dialog.open(ModalCompleteComponent, { data: modalConf }).afterClosed()),
+      )
+      .subscribe(() => {
+        this.load = false;
+        this.router.navigate(['/home/reception']);
+      });
+  }
+
+  updateSymbol(symbol: string) {
+    this.symbol = symbol;
+  }
+
+  private generateExchangeReq(guest: GuestEntity, money: string, value: string): ExchangeReqModel {
+    return {
+      guest: guest.uid,
+      hotel: guest.hotel.uid,
+      money,
+      value,
+    };
   }
 }
