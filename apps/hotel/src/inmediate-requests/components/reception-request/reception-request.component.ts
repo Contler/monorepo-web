5f769402baacab158ca9f2613db94475be8a0096
Jose G Vargas <ruedagato@gmail.com>
2020-10-07 21:58:48 -0500

    reception request

---
+++ apps/hotel/src/inmediate-requests/components/reception-request/reception-request.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contler-reception-request',
+  templateUrl: './reception-request.component.html',
+  styleUrls: ['./reception-request.component.scss'],
+})
+export class ReceptionRequestComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
