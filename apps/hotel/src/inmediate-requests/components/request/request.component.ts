63703fb3de0258178c5b17c698a295bf91e3388f
Jose G Vargas <ruedagato@gmail.com>
2020-10-07 21:48:02 -0500

    create request component

---
+++ apps/hotel/src/inmediate-requests/components/request/request.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contler-request',
+  templateUrl: './request.component.html',
+  styleUrls: ['./request.component.scss'],
+})
+export class RequestComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
