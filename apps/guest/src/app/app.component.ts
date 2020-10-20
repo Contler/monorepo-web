1fb2168dc7cff6872080d0a8df17fe717306a4dc
Andres-Tejada <andrew081889@hotmail.com>
2020-10-20 08:58:38 -0500

    Ajuste para verificar si el huesped tiene la fecha de salida vigente cuando se recarga la pagina

--- apps/guest/src/app/app.component.ts
+++ apps/guest/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GuestService } from 'guest/services/guest.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'contler-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'guest';
+
+  constructor(private guestService: GuestService, private afAuth: AngularFireAuth, private router: Router) {}
+
+  ngOnInit(): void {
+    this.avalibleUser();
+  }
+
+  avalibleUser() {
+    this.guestService.checkAvailableUser().subscribe(({ checkIn, checkOut }) => {
+      if (new Date() < checkIn) {
+        this.afAuth
+          .signOut()
+          .then(() => this.router.navigate(['/login']))
+          .catch(() => {});
+      } else if (new Date() > checkOut) {
+        this.afAuth
+          .signOut()
+          .then(() => this.router.navigate(['/login']))
+          .catch(() => {});
+      } else {
+        this.router.navigate(['/home']);
+      }
+    });
+  }
 }
