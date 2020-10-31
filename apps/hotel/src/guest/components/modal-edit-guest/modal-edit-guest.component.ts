b324281bdbbaa6ffa6bb97b3e24e93b5a0a55633
Andres-Tejada <andrew081889@hotmail.com>
2020-10-30 22:27:32 -0500

    Ajustes de componente

--- apps/hotel/src/guest/components/modal-edit-guest/modal-edit-guest.component.ts
+++ apps/hotel/src/guest/components/modal-edit-guest/modal-edit-guest.component.ts
@@ -1,18 +1,26 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { GuestEntity } from '@contler/entity/guest.entity';
 import { GuestService } from 'hotel/guest/services/guest.service';
 import { MessagesService } from 'hotel/services/messages/messages.service';
+import { SpecialRequestsService } from 'hotel/special-requests/services/special-requests.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'contler-modal-edit-guest',
   templateUrl: './modal-edit-guest.component.html',
   styleUrls: ['./modal-edit-guest.component.scss'],
 })
-export class ModalEditGuestComponent {
+export class ModalEditGuestComponent implements OnInit, OnDestroy {
   load = false;
   guestGroup: FormGroup;
+  private specialRequestsSubscription: Subscription | null = null;
+  dataSource = new MatTableDataSource<any>([]);
+  displayedColumns: string[] = ['userName', 'roomName', 'description', 'checkIn', 'checkOut'];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator | undefined;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private data: GuestEntity,
@@ -20,6 +28,7 @@ export class ModalEditGuestComponent {
     public dialogRef: MatDialogRef<ModalEditGuestComponent>,
     private guestService: GuestService,
     private messagesService: MessagesService,
+    private specialRequestsService: SpecialRequestsService,
   ) {
     this.guestGroup = formBuild.group({
       name: [data.name, Validators.required],
@@ -28,6 +37,21 @@ export class ModalEditGuestComponent {
       checkIn: [data.checkIn, Validators.required],
       checkOut: [data.checkOut, Validators.required],
     });
+  }
+
+  ngOnInit() {
+    this.getSpecialRequestByHotel();
+  }
+
+  getSpecialRequestByHotel() {
+    this.specialRequestsSubscription = this.specialRequestsService
+      .listenSpecialRequestByHotel()
+      .subscribe((requests) => {
+        if (requests) {
+          this.dataSource.data = requests.filter((req) => req.guest.uid === this.data.uid);
+          this.dataSource.paginator = this.paginator as MatPaginator;
+        }
+      });
   }
 
   saveUser() {
@@ -46,5 +70,11 @@ export class ModalEditGuestComponent {
         this.messagesService.showServerError();
       },
     );
+  }
+
+  ngOnDestroy() {
+    if (this.specialRequestsSubscription) {
+      this.specialRequestsSubscription.unsubscribe();
+    }
   }
 }
