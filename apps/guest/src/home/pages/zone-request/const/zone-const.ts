aeacbff880c35b473057de59737dc83fab4018aa
Andres-Tejada <andrew081889@hotmail.com>
2020-11-03 17:43:03 -0500

    Ajuste de componente

--- apps/guest/src/home/pages/zone-request/const/zone-const.ts
+++ apps/guest/src/home/pages/zone-request/const/zone-const.ts
@@ -1,2 +1,24 @@
 export const SUB_CATEGORY_DRINKS = 'Bebidas';
 export const ROOM_SERVICE = 'Room Service';
+export const CATEGORY_ZONE_EN = [
+  {
+    id: 1,
+    name: 'SOCIAL AREAS',
+  },
+  {
+    id: 2,
+    name: 'HUMID AREAS',
+  },
+  {
+    id: 3,
+    name: 'MY ROOM',
+  },
+  {
+    id: 4,
+    name: 'OUTSIDE THE HOTEL',
+  },
+  {
+    id: 5,
+    name: 'FOOD AREAS',
+  },
+];
