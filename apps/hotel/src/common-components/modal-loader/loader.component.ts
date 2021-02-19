7b9a4668e47d6ebdc63216aad5d187d4ae447747
Joan Serna <jsernaleiton@gmail.com>
2021-02-19 12:42:44 -0500

    :bug: fix error typo

--- apps/hotel/src/common-components/modal-loader/loader.component.ts
+++ apps/hotel/src/common-components/modal-loader/loader.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'fave-loader',
+  selector: 'contler-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss'],
 })
