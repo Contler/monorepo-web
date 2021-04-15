52f79f6be6698f3bdb0d90f88db26e91a09bf5c1
Joan Serna <jsernaleiton@gmail.com>
2021-04-15 11:49:52 -0500

    :zap: guest can update status dynamic form

--- apps/guest/src/my-request/my-request.module.ts
+++ apps/guest/src/my-request/my-request.module.ts
@@ -9,6 +9,8 @@ import { RequestItemComponent } from './components/request-item/request-item.com
 import { DynamicTranslateModule } from '@contler/dynamic-translate';
 import { DetailRequestComponent } from './pages/detail-request/detail-request.component';
 import { DynamicServicesModule } from '@contler/dynamic-services';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MyRequestComponent, RequestItemComponent, DetailRequestComponent],
@@ -19,6 +21,8 @@ import { DynamicServicesModule } from '@contler/dynamic-services';
     TranslateModule,
     DynamicTranslateModule,
     DynamicServicesModule,
+    MaterialModule,
+    FormsModule,
   ],
 })
 export class MyRequestModule {}
--- apps/guest/src/my-request/pages/detail-request/detail-request.component.ts
+++ apps/guest/src/my-request/pages/detail-request/detail-request.component.ts
@@ -1,25 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
-import { DynamicRequest, receptionDynamicConverter } from '@contler/dynamic-services';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { DynamicRequest, DynamicRequestStatus, receptionDynamicConverter } from '@contler/dynamic-services';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { MessagesService } from '../../../services/messages/messages.service';
 
 @Component({
   selector: 'contler-detail-request',
   templateUrl: './detail-request.component.html',
   styleUrls: ['./detail-request.component.scss'],
 })
-export class DetailRequestComponent implements OnInit {
+export class DetailRequestComponent {
   request: Observable<DynamicRequest>;
+  listStatus = [
+    DynamicRequestStatus.PROGRAMING,
+    DynamicRequestStatus.ATTENDED,
+    DynamicRequestStatus.COMPLETED,
+  ];
+  statusRequest = DynamicRequestStatus;
+  hiddenButton = false;
 
-  constructor(private route: ActivatedRoute, private db: AngularFirestore) {
+  constructor(
+    private route: ActivatedRoute,
+    private db: AngularFirestore,
+    private messagesService: MessagesService,
+  ) {
     const reference = this.db.firestore.collection('dynamicRequest').withConverter(receptionDynamicConverter);
     this.request = this.route.params.pipe(
       map(({ id }) => id),
       switchMap((id) => this.db.collection<DynamicRequest>(reference).doc(id).valueChanges()),
+      tap((request) => (this.hiddenButton = request.status === this.statusRequest.COMPLETED)),
     );
   }
 
-  ngOnInit(): void {}
+  public async onUpdateRequest(rq: DynamicRequest): Promise<void> {
+    const reference = this.db.firestore.collection('dynamicRequest').withConverter(receptionDynamicConverter);
+    const loader = this.messagesService.showLoader();
+    try {
+      await this.db.collection<DynamicRequest>(reference).doc(rq.key).update(rq);
+      this.hiddenButton = true;
+      this.messagesService.closeLoader(loader);
+    } catch (e) {
+      this.messagesService.closeLoader(loader);
+      this.messagesService.showServerError();
+    }
+  }
 }
