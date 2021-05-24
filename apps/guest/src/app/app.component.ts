f65b96b593a10ffdf9c7d5435debf7b3f5803fcf
Jose G Vargas <ruedagato@gmail.com>
2021-05-24 09:57:14 -0500

    update welcome

--- apps/guest/src/app/app.component.ts
+++ apps/guest/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { environment } from 'guest/environments/environment';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'contler-root',
@@ -9,9 +10,10 @@ import { environment } from 'guest/environments/environment';
 })
 export class AppComponent {
   title = 'guest';
-  constructor(db: AngularFireDatabase) {
+  constructor(db: AngularFireDatabase, fire: AngularFirestore) {
     if (environment.emulate) {
       db.database.useEmulator('localhost', 9000);
+      fire.firestore.useEmulator('localhost', 8081);
     }
   }
 }
