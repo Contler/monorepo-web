28712464bb4ead3bf9cebd7b45b7686c9df5204a
Jose G Vargas <ruedagato@gmail.com>
2021-05-26 17:59:29 -0500

    fix space support

--- apps/guest/src/common-components/option-grid/option-grid.component.ts
+++ apps/guest/src/common-components/option-grid/option-grid.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'contler-option-grid',
   templateUrl: './option-grid.component.html',
   styleUrls: ['./option-grid.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class OptionGridComponent implements OnInit {
   constructor() {}
